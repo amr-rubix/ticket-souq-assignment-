223142bc9a0a1b3befd6836a21e6d99091e12fff
Amr Ismail <amr@rubix-studio.com>
2024-07-27 16:04:10 +0300

    fix type error

--- redis-events/src/app.controller.ts
+++ redis-events/src/app.controller.ts
@@ -1,12 +1,13 @@
 import { Controller } from '@nestjs/common';
 import { AppService } from './app.service';
 import { EventPattern } from '@nestjs/microservices';
+import { User } from './types';
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @EventPattern('user_created')
-  async handleUserCreated(data: Record<string, unknown>) {
+  async handleUserCreated(data: User): Promise<boolean> {
     // business logic
     return this.appService.sendUserCreatedNotification(data);
   }
